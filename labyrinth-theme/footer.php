5be7cce0f27ee8429d6d383c8bc75b5181a3a59e
sjw-steven <71651948+sjw-steven@users.noreply.github.com>
2022-08-19 14:35:12 +0800

    Add files via upload

---
+++ labyrinth-theme/footer.php
@@ -0,0 +1,13 @@
+<div class="footer-container">
+    <div class="footer-content">
+        <div class="footer-content-title">
+            <?php bloginfo('description'); ?>
+        </div>
+        <div class="footer-content-descr">
+            <?php bloginfo('name'); ?>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
